dc8b630ae9fae7841fc046a859a1e5272cabf298
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2013-09-14 00:23:23 +0800

    Cleanup

--- src/org/httpkit/server/RackHttpHandler.java
+++ src/org/httpkit/server/RackHttpHandler.java
@@ -16,35 +16,5 @@ public class RackHttpHandler implements Runnable {
     }
 
     public void run() {
-//      System.out.println("RackHttpHandler run");
-//
-//        try {
-//            Object resp_arr[] = handler.call(buildRequestMap(req));
-//
-//            if (resp_arr == null) { // handler return null
-//                cb.run(HttpEncode(404, new HeaderMap(), null));
-//            } else {
-//                Map<Keyword, Object> resp = new TreeMap<Keyword, Object>();
-//
-//                resp.put(STATUS  , resp_arr[0]);
-//                resp.put(HEADERS , PersistentArrayMap.create((Map) resp_arr[1]));
-//                resp.put(BODY    , resp_arr[2]);
-//
-//                System.out.println( resp.get(STATUS) );
-//
-//                Object body = resp.get(BODY);
-//
-//                if (!(body instanceof AsyncChannel)) { // hijacked
-//                    HeaderMap headers = HeaderMap.camelCase((Map) resp.get(HEADERS));
-//                    if (req.version == HTTP_1_0 && req.isKeepAlive) {
-//                        headers.put("Connection", "Keep-Alive");
-//                    }
-//                    cb.run(HttpEncode(getStatus(resp), headers, body));
-//                }
-//            }
-//        } catch (Throwable e) {
-//            cb.run(HttpEncode(500, new HeaderMap(), e.getMessage()));
-//            HttpUtils.printError(req.method + " " + req.uri, e);
-//        }
     }
 }
